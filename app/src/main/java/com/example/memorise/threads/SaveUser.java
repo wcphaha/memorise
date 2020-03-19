4e8fd9c68c63d2fa4236fbc1b24405ccabec6f7a
wcphaha <1683168521@qq.com>
2020-03-19 21:54:34 +0800

    设置界面完成

---
+++ app/src/main/java/com/example/memorise/threads/SaveUser.java
@@ -0,0 +1,35 @@
+package com.example.memorise.threads;
+
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.memorise.StaticVar.Variable;
+import com.example.memorise.sql.MyDataBase;
+
+public class SaveUser extends Thread {
+    SQLiteDatabase db;
+
+    public SaveUser(SQLiteDatabase db){
+        this.db = db;
+    }
+    @Override
+    public void run() {
+        super.run();
+        init();
+    }
+    void init(){
+        MyDataBase mdb = new MyDataBase(db);
+
+        mdb.update("update user set password = '"+ Variable.user.password +"'");
+
+        mdb.update("update user set name = '"+ Variable.user.name +"'");
+
+        mdb.update("update user set dayplan = '"+ Variable.user.dayplan +"'");
+
+        mdb.update("update user set email = '"+ Variable.user.email +"'");
+
+        mdb.update("update user set sign = '"+ Variable.user.sign +"'");
+
+        mdb.update("update user set address = '"+ Variable.user.address +"'");
+
+    }
+}
